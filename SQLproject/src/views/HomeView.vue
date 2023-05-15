8524bd94cd77fe541e09944b88794e0bdff330fb
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2023-05-15 10:15:06 -0400

    New Draft

---
+++ SQLproject/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="header">
+      <h1 class="text">Book depository</h1>
+
+      <p>
+        This page will have a display with the top seller books? then there is a search function to
+        find books api: google Books? -> has a title search funtion Then the user can make an
+        account and like the book -> read the book? -> want to read?
+      </p>
+
+      <p>Account creation -> User name and then ID -> account name. Then there is a log in page</p>
+
+      <p>
+        Once that is developed there should be 1 base component that pulls information and makes an
+        account page for every user by pushing information it also displays the books that the user
+        has chosen before hand -> the user can delete books and their account
+      </p>
+
+      <h2>account catalog??</h2>
+
+      <h3>find a way to disply information from the table first and then add more items??</h3>
+      <p>I love coding</p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
